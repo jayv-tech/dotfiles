8710678d4e6860f477ffeef277b012b1ae7efef8
Jayavel S <101336634+jayv-tech@users.noreply.github.com>
2025-01-14 14:06:49 +0530

    Adding Eza commands

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -3,10 +3,14 @@ Invoke-Expression (&starship init powershell)
 # Setting aliases for eza
 function Show-EzaList { eza --icons --group-directories-first @args }
 function Show-EzaListLong { eza --icons --group-directories-first -l @args }
+function Show-EzaHidList { eza -lTag --level=1 --icons --group-directories-first @args }
+function Show-EzaTier2List { eza -lTag --level=2 --icons --group-directories-first @args }
 
 # Set PowerShell aliases to these functions
 Set-Alias -Name ls -Value Show-EzaList
 Set-Alias -Name ll -Value Show-EzaListLong
+Set-Alias -Name lh -Value Show-EzaHidList
+Set-Alias -Name lt2 -Value Show-EzaTier2List
 
 # Setting aliases for yt-dlp
 function dvid {
